a85037b1b43bab54ea4f46bde5bf08d0a1200e08
Kexy Biscuit <kexybiscuit@biscuitt.in>
2021-10-15 22:22:08 +0800

    Update docker-entrypoint.sh
    [skip ci]

--- node/14/impish/docker-entrypoint.sh
+++ node/14/impish/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Adapted from https://github.com/nodejs/docker-node/blob/961785b30d9a7cf1d46271e1b71fe0c68be44cff/14/bullseye/docker-entrypoint.sh
 set -e
 
 if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
